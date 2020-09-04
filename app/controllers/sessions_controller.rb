938450406dbfb49b101d75b11ab0b891786836aa
Natalia <n8galan@gmail.com>
2020-09-03 19:35:08 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    name = params[:name]
+    if name && name.length > 0
+      session[:name] = name
+      redirect_to secrets_path
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session.delete(:name) if session[:name]
+
+    redirect_to login_path
+  end
+
+end #end of sc class
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: redirect('/login')
+get '/login', to: 'sessions#new', as: 'login'
+post '/login', to: 'sessions#create'
+post '/logout', to: 'sessions#destroy'
+
+resource :secrets, only: [:show]
+
 end
